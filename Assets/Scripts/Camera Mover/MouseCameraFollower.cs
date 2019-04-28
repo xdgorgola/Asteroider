f12c9d0acf02abd88fdba51c0757edef1f4bd053
xdgorgola <15-11264@usb.ve>
2019-04-27 20:12:18 -0400

    Camera/Mouse smooth follower included

--- Assets/Scripts/Camera Mover/MouseCameraFollower.cs
+++ Assets/Scripts/Camera Mover/MouseCameraFollower.cs
@@ -2,127 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class MouseCameraFollower : MonoBehaviour
 {
     /// <summary>
     /// Main camera
     /// </summary>
-    private Camera camera;
+    private Camera cam;
     /// <summary>
     /// Mouse worldposition
     /// </summary>
     private Vector3 mousePos = Vector3.zero;
-    private Vector3 offset = new Vector3(0f, 0f, 0f);
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private Vector3 mouseOffset = Vector3.zero;
     /// <summary>
-    /// Player transform
+    /// target transform
     /// </summary>
     [SerializeField]
-    private Transform playerPos;
+    private Transform target;
 
-    /// <summary>
-    /// Player to camera inner radius.
-    /// </summary>
-    [SerializeField]
-    private float pcInnerRadius = 5f;
-    /// <summary>
-    /// Player to camera exterior radius
-    /// </summary>
-    [SerializeField]
-    private float pcExteriorRadius = 6f;
-    /// <summary>
-    /// Camera to mouse inner radius
-    /// </summary>
-    [SerializeField]
-    private float cmInnerRadius = 7f;
-    /// <summary>
-    /// Camera to mouse exterior radius
-    /// </summary>
-    [SerializeField]
-    private float cmExteriorRadius = 8f;
     /// <summary>
     /// Camera movement speed
     /// </summary>
     [SerializeField]
     private float cameraSpeed = 2f;
 
-    public GameObject testSphere;
-    
+    [SerializeField]
+    [Range(0,1)]
+    private float smoothAmout = 0.125f;
 
-    //NOTES
-    //Some variables are not used, check them and delete them
-
-        //No hace lo que quiero, necesito que la camara este en la direccion del mouse.
+    private Vector3 velocity = Vector3.zero;
 
     private void Awake()
     {
-        camera = Camera.main;
-        Debug.Log(Input.mousePosition);
+        cam = Camera.main;
+        //Debug.Log(Input.mousePosition);
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
-        //Distance betwheen the mouse and the camera
-        float mouseDistance = ((mousePos - transform.position)).magnitude;
-        
-        Vector3 direction = ((mousePos - transform.position)).normalized;
-
-
-        Vector3 directionMouse = (mousePos - playerPos.position + (Vector3.forward * 10)).normalized;
-        Vector3 desiredPosition = playerPos.position + directionMouse * (pcExteriorRadius) - (Vector3.forward * 10);
-        testSphere.transform.position = desiredPosition + (Vector3.forward * 10);
-
-        //Debug.Log("Mouse pos: " + mousePos + " Camera pos: " + transform.position);
-        //Debug.Log("Player pos: " + playerPos.position + " directionMouse: " + directionMouse);
-
-        direction = (desiredPosition - transform.position).normalized;
-        //Debug.Log(direction);
-        //Distance between the camera and the player
-        float cameraDistance = (transform.position - playerPos.position + (Vector3.forward * 10)).magnitude;
-        //Debug.Log("Desired: " + desiredPosition);
-        //Debug.Log(transform.position - playerPos.position + (Vector3.forward * 10));
-        //Checks if the camera is in the allowed movement range and if the mouse is past the camera inner radius to make it move
-        if (mouseDistance > cmInnerRadius) //&& cameraDistance < pcExteriorRadius)
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        Vector3 direction = (mousePos - target.position).normalized;
+        float distance;
+        if ((mousePos - target.position).magnitude < 4)
         {
-            Vector3 finalVector;
-            float newCameraDistance = (transform.position + (direction * cameraSpeed) + Vector3.forward * 10).magnitude;
-            //Debug.Log(transform.position + (direction * cameraSpeed) + Vector3.forward * 10);
-            if (newCameraDistance > pcExteriorRadius - cameraDistance)
-            {
-                Debug.Log("No se puede");
-                //finalVector = direction * (pcExteriorRadius - cameraDistance) * Time.deltaTime;
-                transform.position = desiredPosition;
-            }
-            
-            else if (newCameraDistance < pcExteriorRadius - cameraDistance)
-            {
-                Debug.Log("Se puede");
-                finalVector = direction * cameraSpeed * Time.deltaTime;
-                transform.Translate(finalVector);
-            }
-            else
-            {
-                Debug.Log("Ya esta");
-                
-            }
-            //Debug.Log(finalVector);
-            
+            distance = 0;
         }
         else
         {
+            distance = Mathf.Clamp((mousePos - target.position).magnitude, 0f, 10f);
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        //Gizmos.color = Color.blue;
-        //Gizmos.DrawWireSphere(playerPos.position, pcInnerRadius);
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(playerPos.position, pcExteriorRadius);
-
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position + Vector3.forward * 10, cmInnerRadius);
-        //Gizmos.color = Color.magenta;
-        //Gizmos.DrawWireSphere(transform.position + Vector3.forward * 10, cmExteriorRadius);
+        
+        Vector3 desiredPosition = target.position + offset + (direction * distance) + mouseOffset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothAmout);
+        transform.position = smoothedPosition;
     }
 }
